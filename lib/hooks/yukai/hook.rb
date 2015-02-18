1216c63d510f85e2188a622c4c81a20961344c62
SuzukiMasashi <suzuki_masasi@nexway.co.jp>
2015-02-18 14:49:16 +0900

    Modify method name

--- lib/hooks/yukai/hook.rb
+++ lib/hooks/yukai/hook.rb
@@ -1,7 +1,7 @@
 module Idobata::Hook
   class Yukai < Base
-    screen_name '夕会'
-    identifier  :Yukai
-    icon_url    'http://suzukimasashi.github.io/assets/images/SKE48/kato_rumi/00004.jpg'
+    name       '夕会'
+    identifier :yukai
+    icon_url   'http://suzukimasashi.github.io/assets/images/SKE48/kato_rumi/00004.jpg'
   end
 end
